b9928a31e29b1adcfc4b1d35e91b6fdb5d4773e3
Uzi-SaGor-01 <jahidulislam2782466@gmail.com>
2025-08-22 23:03:47 +0600

    Update busy.js

--- script/commands/busy.js
+++ script/commands/busy.js
@@ -1,49 +1,52 @@
 module.exports.config = {
-	name: "مشغول",
+	name: "busy",
 	version: "1.0.0",
 	permissions: 0,
-	credits: "عمر",
-	description: "قم بتشغيل وضع الانشغال أو إيقاف تشغيله",
-  usages: "ا",
-  commandCategory: "خدمات",
-  cooldowns: 5
+	credits: "SaGor",
+	description: "Enable or disable busy mode",
+	usages: "use",
+	commandCategory: "services",
+	cooldowns: 5
 };
 
 const busyPath = __dirname + '/cache/busy.json';
 const fs = require('fs');
 
 module.exports.onLoad = () => {
-  if (!fs.existsSync(busyPath)) fs.writeFileSync(busyPath, JSON.stringify({}));
+	if (!fs.existsSync(busyPath)) fs.writeFileSync(busyPath, JSON.stringify({}));
 }
 
 module.exports.handleEvent = async function({ api, event, Users }) {
     let busyData = JSON.parse(fs.readFileSync(busyPath));
     var { senderID, threadID, messageID, mentions } = event;
+
     if (senderID in busyData) {
         var info = busyData[senderID];
         delete busyData[senderID];
         fs.writeFileSync(busyPath, JSON.stringify(busyData, null, 4));
-        return api.sendMessage(`مرحبًا بعودتك!!`, threadID, () => {
-            if (info.tag.length == 0) api.sendMessage("محد سوالك تاك !", threadID);
+        return api.sendMessage(`Welcome back!!`, threadID, () => {
+            if (info.tag.length == 0) api.sendMessage("No one mentioned you while you were away!", threadID);
             else {
                 var msg = "";
                 for (var i of info.tag) {
                     msg += `${i}\n`
                 }
-                api.sendMessage("فيما يلي قائمة بلأشخاص الي سوولك تاك أثناء غيابك:\n\n" + msg, threadID)
+                api.sendMessage("Here is the list of people who mentioned you while you were away:\n\n" + msg, threadID)
             }
         }, messageID);
     }
 
     if (!mentions || Object.keys(mentions).length == 0) return;
-    
+
     for (const [ID, name] of Object.entries(mentions)) {
         if (ID in busyData) {
-            var infoBusy = busyData[ID], mentioner = await Users.getNameUser(senderID), replaceName = event.body.replace(`${name}`, "");
-            infoBusy.tag.push(`${mentioner}: ${replaceName == "" ? "سوالك تاك مرة وحدة" : replaceName}`)
+            var infoBusy = busyData[ID];
+            var mentioner = await Users.getNameUser(senderID);
+            var replaceName = event.body.replace(`${name}`, "");
+            infoBusy.tag.push(`${mentioner}: ${replaceName == "" ? "mentioned you once" : replaceName}`);
             busyData[ID] = infoBusy;
             fs.writeFileSync(busyPath, JSON.stringify(busyData, null, 4));
-            return api.sendMessage(`${name.replace("@", "")} مشغول حاليا ${infoBusy.lido ? ` , السبب : ${infoBusy.lido}.` : "."}`, threadID, messageID);
+            return api.sendMessage(`${name.replace("@", "")} is currently busy${infoBusy.lido ? `, Reason: ${infoBusy.lido}.` : "."}`, threadID, messageID);
         }
     }
 }
@@ -51,16 +54,16 @@ module.exports.handleEvent = async function({ api, event, Users }) {
 module.exports.run = async function({ api, event, args, Users }) {
 	await new Promise(resolve => setTimeout(resolve, 1000));
     let busyData = JSON.parse(fs.readFileSync(busyPath));
-    const { threadID, senderID, messageID, body } = event;
+    const { threadID, senderID, messageID } = event;
     var content = args.join(" ") || "";
+
     if (!(senderID in busyData)) {
         busyData[senderID] = {
             lido: content,
             tag: []
         }
         fs.writeFileSync(busyPath, JSON.stringify(busyData, null, 4));
-       var msg = (content.length == 0) ? 'لقد قمت بتشغيل وضع مشغول بدون سبب' : `لقد قمت بتشغيل وضع مشغول , السبب: ${content}`;
+        var msg = (content.length == 0) ? 'You have enabled busy mode without a reason' : `You have enabled busy mode, Reason: ${content}`;
         return api.sendMessage(msg, threadID, messageID);
     }
 }
-
