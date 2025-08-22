0b7730368580d5b3de319401a2f314dac1844731
Uzi-SaGor-01 <jahidulislam2782466@gmail.com>
2025-08-22 22:52:15 +0600

    Update Kickme.js

--- script/commands/Kickme.js
+++ script/commands/Kickme.js
@@ -1,21 +1,22 @@
 module.exports.config = {
-name: "اطرديني",
-version: "1.0.0",
-hasPermssion: 0,
-credits: "عمر",
-description: "تطردك من المجموعة",
-commandCategory: "خدمات",
-usages: "  اذا انت ادمن ومتريد الامر يشتغل ببساطه نزل البوت من الادمن",
-cooldowns: 3
+    name: "kickme",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "SaGor",
+    description: "Kick yourself from the group",
+    commandCategory: "services",
+    usages: "If you are admin and want this command to work, simply make the bot an admin",
+    cooldowns: 3
 }; 
 
 module.exports.run = async function({ api, event, args }) {
-var info = await api.getThreadInfo(event.threadID);
-if (!info.adminIDs.some(item => item.id == api.getCurrentUserID())) return api.sendMessage('هات ادمن وتدلل ', event.threadID, event.messageID);
-var threadInfo = await api.getThreadInfo(event.threadID)
-    {
-    
-   api.removeUserFromGroup(event.senderID, event.threadID)
-    }
-api.sendMessage(`امرك ابلع`, event.threadID);
+    var info = await api.getThreadInfo(event.threadID);
+    if (!info.adminIDs.some(item => item.id == api.getCurrentUserID())) 
+        return api.sendMessage('Make me admin first!', event.threadID, event.messageID);
+
+    var threadInfo = await api.getThreadInfo(event.threadID);
+    
+    api.removeUserFromGroup(event.senderID, event.threadID);
+    
+    api.sendMessage(`Your request is done!`, event.threadID);
 }
