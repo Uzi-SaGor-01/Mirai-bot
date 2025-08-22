2aa160bbc726d889c24039f15cfb37a19fbf3c3b
Uzi-SaGor-01 <jahidulislam2782466@gmail.com>
2025-08-22 22:46:09 +0600

    Update autosetname.js

--- script/commands/autosetname.js
+++ script/commands/autosetname.js
@@ -1,11 +1,11 @@
 module.exports.config = {
-    name: "كنية",
+    name: "nickname",
     version: "1.0.1",
     hasPermssion: 1,
-    credits: "عمر",
-    description: "يخلي كنية لاي عضو ينضم",
-    commandCategory: "مسؤولي المجموعات",
-    usages: "[اضف <الاسم> /حذف] ",
+    credits: "Omar",
+    description: "Sets a nickname for any new member",
+    commandCategory: "group_admin",
+    usages: "[add <name> / remove]",
     cooldowns: 5
 }
 
@@ -26,28 +26,25 @@ module.exports.run = async function  ({ event, api, args, permssionm, Users })  
     var dataJson = JSON.parse(readFileSync(pathData, "utf-8"));
     var thisThread = dataJson.find(item => item.threadID == threadID) || { threadID, nameUser: [] };
     switch (args[0]) {
-        case "اضف":
         case "add": {
-            if (content.length == 0) return api.sendMessage("لا يمكن ترك قسم  تعين الكنية للعضو الجديد فارغًا!", threadID, messageID);
-            if (thisThread.nameUser.length > 0) return api.sendMessage("يرجى حذف  الكنية القديم قبل تعيين اسم جديد!!!", threadID, messageID); 
+            if (content.length == 0) return api.sendMessage("Cannot leave nickname section empty!", threadID, messageID);
+            if (thisThread.nameUser.length > 0) return api.sendMessage("Please remove the old nickname before setting a new one!", threadID, messageID); 
             thisThread.nameUser.push(content);
             const name = (await Users.getData(event.senderID)).name
             writeFileSync(pathData, JSON.stringify(dataJson, null, 4), "utf-8");
-            api.sendMessage(`تم حفظ تعيين الكنية تلقائيا للضو الجديد بنجاح \nمعاينة : ${content}\nمنشئ الكنية: ${name}`, threadID, messageID);
+            api.sendMessage(`Successfully saved automatic nickname for new members\nPreview: ${content}\nNickname creator: ${name}`, threadID, messageID);
             break;
         }
-        case "حذف":
-        case "remove":
-        case "delete": {
-                if (thisThread.nameUser.length == 0) return api.sendMessage("لم تقم مجموعتك بتكوين كنية للاعضاء الجدد حتى الآن!!", threadID, messageID);
+        case "remove": {
+                if (thisThread.nameUser.length == 0) return api.sendMessage("Your group hasn't set up a nickname for new members yet!", threadID, messageID);
                 thisThread.nameUser = [];
-                api.sendMessage(`تم حذف الكنية بنجاح`, threadID, messageID);
+                api.sendMessage(`Nickname removed successfully`, threadID, messageID);
                 break;
         }
         default: {
-                api.sendMessage(`الاستخدام: كنية اضف <الاسم> لتكوين الألقاب للأعضاء الجدد\nDùng: كنية حذف لإزالة الألقاب للأعضاء الجدد`, threadID, messageID);
+                api.sendMessage(`Usage: nickname add <name> to set nickname for new members\nUsage: nickname remove to remove nickname for new members`, threadID, messageID);
         }
     }
     if (!dataJson.some(item => item.threadID == threadID)) dataJson.push(thisThread);
     return writeFileSync(pathData, JSON.stringify(dataJson, null, 4), "utf-8");
-        }
+}
